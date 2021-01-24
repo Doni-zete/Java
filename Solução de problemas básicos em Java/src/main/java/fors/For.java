015860c708ad45c8037223ba82d603d340763265
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-01-24 17:27:21 -0300

    solucao

---
+++ Solução de problemas básicos em Java/src/main/java/fors/For.java
@@ -0,0 +1,30 @@
+
+package fors;
+
+import java.util.Scanner;5
+public class For {
+    public static void main(String[] argss){
+        Scanner ent = new Scanner(System.in);
+        int num, contPar = 0, contImpar = 0;
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Digite o " + i + "º número");
+            num = ent.nextInt();
+
+            if(num % 2 == 0){
+                contPar++;
+            }
+
+            // para informar números impares
+            /*
+            if(num % 2 == 1){
+                contImpar++;
+            }
+            */
+        }
+
+        System.out.println("Foram digitados " + contPar + " números pares \n");
+        // System.out.println("E " + contImpar + " números ímpares");
+
+    }
+}
