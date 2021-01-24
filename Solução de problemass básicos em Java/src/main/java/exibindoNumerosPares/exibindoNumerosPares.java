ec7577b8a3b92d4defd9b594de04fa3bc0ec960c
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-01-24 17:44:57 -0300

    solucao

---
+++ Solução de problemass básicos em Java/src/main/java/exibindoNumerosPares/exibindoNumerosPares.java
@@ -0,0 +1,15 @@
+package exibindoNumerosPares;
+
+public class exibindoNumerosPares {
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0 ; i <= 10 ; i++) {
+            if (i) System.out.println(i);
+        }
+    }
+
+}
+
+
+
+
