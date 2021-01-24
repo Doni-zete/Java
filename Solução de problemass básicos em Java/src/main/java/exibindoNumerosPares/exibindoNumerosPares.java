36e25005fb1cd069487512dbade5d511761d0034
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-01-24 18:30:25 -0300

    solucao

--- Solução de problemass básicos em Java/src/main/java/exibindoNumerosPares/exibindoNumerosPares.java
+++ Solução de problemass básicos em Java/src/main/java/exibindoNumerosPares/exibindoNumerosPares.java
@@ -1,15 +1,37 @@
-package exibindoNumerosPares;
+/*
+Desafio
+
+        Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+        Entrada
+
+        Você receberá 1 valor inteiro N, onde N > 0.
+        Saída
+
+        Exiba todos os números pares até o valor de entrada, sendo um em cada linha.
+*/
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class exibindoNumerosPares {
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0 ; i <= 10 ; i++) {
-            if (i) System.out.println(i);
+
+        String num;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        num = br.readLine();
+        for (int i = 2; i <= Integer.parseInt(num); i++) {
+
+            if (i % 2 == 0) {
+
+                System.out.println(i);
+
+            }
+
         }
     }
-
 }
-
-
-
-
